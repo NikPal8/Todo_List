7d9b5a54b8ea7d9c44155d705c5fde704cd6907e
Nik <nik_palibrk@hotmail.com>
2023-10-04 13:06:47 +1100

    Added a delete button, to the right of the li and gave it some stykes. Changed the span to which carries the value of the input box to a paragraph. Created a span to left of the li which adds numbers to every list item. Currently there is no functionality in the delete button.

--- Todo_List.js
+++ Todo_List.js
@@ -6,23 +6,42 @@ const todoTaskInput = document.querySelector('#todoTaskInput');
 
 const todoTask = document.querySelector('#task');
 
-
+let num = 0;
 addBtn.addEventListener('click', function() {
 
-    let newelement = document.createElement('li'); 
-    let taskText = document.createElement('span');
+  num = num + 1;
+        
+    // Span text from input box(#task)
+        let newelement = document.createElement('li'); 
+        let listNumber = document.createElement('span');
+        
+
+        let taskText = document.createElement('p');
+        let taskTextValue = ` ${listNumber.textContent = 0 + num} . ${todoTask.value}`;
+        taskText.textContent = taskTextValue; 
+        // Delete Btn
+        const deleteItem = document.createElement('button');
+        deleteItem.style.color = 'red';
+        deleteItem.textContent = "Delete";
+        
+        
+        
+        todoBody.append(newelement);
+        
+        newelement.insertAdjacentElement('beforebegin', deleteItem); 
+        newelement.append(taskText);
+        
+        newelement.append(deleteItem);
     
-    let taskTextValue = todoTask.value;
-    
-    taskText.textContent = taskTextValue;
-    
-    
-    
-    todoBody.append(newelement);
-    newelement.append(taskText);
     
     
 
     
-})
+});
 
+
+
+
+// deletebtn.addEventListener('click', function(){
+
+// });
